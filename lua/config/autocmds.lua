0f9d3cb2f1b447afb30eceb089e371370f70d80d
Marco Fontani <mf@marcofontani.it>
2024-08-23 10:46:08 +0200

    autocmds: custom highlights for comments

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,9 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.cmd([[
+    autocmd ColorScheme * highlight Comment             term=bold ctermfg=white gui=none guifg=#CAC8FA
+    autocmd ColorScheme * highlight gitCommitComment    term=bold ctermfg=white gui=bold guifg=#505080
+    autocmd ColorScheme * highlight gitCommitUntracked  term=bold ctermfg=white gui=bold guifg=#505080
+]])
+vim.api.nvim_exec_autocmds("ColorScheme", {})
