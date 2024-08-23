eba15da350f177c9a75d6debdd4b25e769e2f3e9
Marco Fontani <mf@marcofontani.it>
2024-08-23 10:46:08 +0200

    keymaps: GrepperGit, hover, etc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", ";", ":", {
+  desc = "DWIM : with ;",
+})
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {
+  desc = "Show LSP Hover info",
+})
+vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {
+  desc = "Show LSP Signature help",
+})
+vim.keymap.set('n', '<Leader>*', '<cmd>Grepper -tool git -cword -noprompt<CR>', {
+  desc = 'Grepper: Git',
+})
+-- vim.keymap.set('c', '%%', '<C-R>=expand("%:p:h") . "/"<CR>', {
+vim.keymap.set('c', '%%', '<C-R>=expand("%:.:h") . "/"<CR>', {
+  desc = 'Insert current file path',
+})
