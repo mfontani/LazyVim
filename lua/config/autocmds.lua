615c21ee34858eccda97741c22b3d3a88082fbdb
Marco Fontani <mf@marcofontani.it>
2024-08-23 10:46:08 +0200

    autocmds: recolor treesitter context

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,8 +2,12 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.cmd([[
+    " Comments
     autocmd ColorScheme * highlight Comment             term=bold ctermfg=white gui=none guifg=#CAC8FA
     autocmd ColorScheme * highlight gitCommitComment    term=bold ctermfg=white gui=bold guifg=#505080
     autocmd ColorScheme * highlight gitCommitUntracked  term=bold ctermfg=white gui=bold guifg=#505080
+    " TreeSitter Context
+    autocmd ColorScheme * highlight TreesitterContextBottom           gui=underline guifg=#6272A4 guibg=#111111
+    autocmd ColorScheme * highlight TreesitterContextLineNumberBottom gui=underline guifg=#6272A4 guibg=#111111
 ]])
 vim.api.nvim_exec_autocmds("ColorScheme", {})
